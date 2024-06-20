16a11397dbdc0e0b2ab991b948a98a2747b36fd4
djovanovic <djovanovic@knowit.rs>
2024-06-20 22:47:49 +0200

    add licencing

--- src/XlsTypewriter/XlsTypewriter/Common/XlsHelper.cs
+++ src/XlsTypewriter/XlsTypewriter/Common/XlsHelper.cs
@@ -5,11 +5,10 @@ public static class XlsHelper
     public static string IntToColumnName(int columnNumber)
     {
         var columnName = string.Empty;
-        int modulo;
 
         while (columnNumber > 0)
         {
-            modulo = (columnNumber - 1) % 26;
+            var modulo = (columnNumber - 1) % 26;
             columnName = Convert.ToChar(65 + modulo) + columnName;
             columnNumber = (columnNumber - modulo) / 26;
         }
